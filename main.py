ba9bcbb365ffab6cab3f45cd47382cb18973eb73
shvedov <shvedovmain@gmail.com>
2026-07-04 18:28:21 +0300

    improvements

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import colors
 import os
 os.system('color')
 
-def loadfile():
+engLang = 1;
+
+def loadfile(name_file):
     words = []
-    with open("test.txt", encoding="utf-8") as file:
+    with open(name_file, encoding="utf-8") as file:
         for line in file:
             parts = line.strip().split('] [')
             if len(parts) == 3:
@@ -16,25 +18,65 @@ def loadfile():
     print("Words loaded:", len(words))
     return words
 
-words = loadfile()
-if words == []:
-    print("File is empty")
-    exit(0)
+while True:
+    print("1. Tests")
+    print("2. Settings")
+    print("3. Exit")
+    choice = int(input("Enter your choice: "))
+    if choice == 3:
+        break
+    if choice == 2:
+        os.system('cls')
+        print("Question in:")
+        print("1. English(default)")
+        print("2. Russian")
+        engLang = int(input("Enter your choice:"))
+    if choice == 1:
+        os.system('cls')
+        print("Enter a name file like <test.txt>")
+        name_file = input()
+        words = loadfile(name_file)
+        if words == []:
+            print("File is empty")
+            exit(0)
 
-random.shuffle(words)
-right = 0
-total = min(10, len(words))
+        random.shuffle(words)
+        right = 0
+        total = min(10, len(words))
 
-for i in range(total):
-    en, trans, ru = words[i]
-    print(f"\n{i + 1}. {ru} ({trans})")
-    ans = input("Answer: ").strip()
+        for i in range(total):
+            en, trans, ru = words[i]
 
-    if ans.lower() == en.lower():
-        print(colors.green("Right"))
-        right += 1
-    else:
-        print(colors.red("Wrong"))
-        print(f"Correct answer: {colors.blue(ru)}")
+            if(engLang == 1):
+                question = en
+                answer = ru
+            else:
+                question = ru
+                answer = en
 
-print(f"\nИтого: {right}/{total}")
+            print(f"\n{i + 1}. {question} ({trans})")
+            ans = input("Answer: ").strip()
+
+            if ans.lower() == answer.lower():
+                print(colors.green("Right"))
+                right += 1
+            else:
+                print(colors.red("Wrong"))
+                print(f"Correct answer: {colors.blue(answer)}")
+
+        print(f"\nTotal: {right}/{total}")
+        choice = 0
+        continue
+
+
+
+
+
+
+
+
+
+
+
+
+
